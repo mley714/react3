ff0298ea498404b1aa5d25c574a35a8e6ce16e5b
mley714 <46652932+mley714@users.noreply.github.com>
2023-02-10 10:56:27 -0800

    initial

---
+++ components/ExpenseItem.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './ExpenseItem.css';
+
+function ExpenseItem() {
+    return (
+        <div className="expense-item">
+            <div>April 13, 2023</div>
+            <div className="expense-item__description">
+                <h2>Car Insurance</h2>
+                <div className="expense-item__price">294.67</div>
+            </div>
+        </div> 
+    );
+}
+export default ExpenseItem;
